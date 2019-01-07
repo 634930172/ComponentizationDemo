b4497028beee643e0cfab302263d9826bea8aa1a
John <634930172@qq.com>
2019-01-07 19:39:23 +0800

    将启动也放到main组件

---
+++ model_main/src/main/java/com/john/mainmodel/SplashAct.java
@@ -0,0 +1,21 @@
+package com.john.mainmodel;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author: John
+ * E-mail: 634930172@qq.com
+ * Date: 2019/1/7 19:23
+ * <p/>
+ * Description:启动相关的页面放到main组件模式下
+ */
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
